33c6fd14f663cd177c00e44e0c7cbe8593483ec5
Shane Friedman <smoores-gpg@friedmans.us>
2026-07-08 18:32:37 -0400

    Formatting

--- src/japanese/transliterate.ts
+++ src/japanese/transliterate.ts
@@ -1,19 +1,19 @@
 import KuromojiAnalyzer from 'kuroshiro-analyzer-kuromoji';
 import Kuroshiro from './kuroshiro';
 
-let kuroshiroInstance : Kuroshiro | null = null
+let kuroshiroInstance: Kuroshiro | null = null;
 
 async function getKuroshiroInstance() {
   if (kuroshiroInstance) {
-    return kuroshiroInstance
+    return kuroshiroInstance;
   }
-  kuroshiroInstance = new Kuroshiro()
-  await kuroshiroInstance.init(new KuromojiAnalyzer())
-  return kuroshiroInstance
+  kuroshiroInstance = new Kuroshiro();
+  await kuroshiroInstance.init(new KuromojiAnalyzer());
+  return kuroshiroInstance;
 }
 
 export async function transliterate(input: string) {
-  const kuroshiro = await getKuroshiroInstance()
+  const kuroshiro = await getKuroshiroInstance();
 
   return await kuroshiro.convert(input);
 }
